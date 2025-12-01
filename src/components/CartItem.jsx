e4963a118279ba3af1370a5327a39d27675aa3df
Lucas Gomes Silva <lucasgomessilva713@gmail.com>
2025-12-01 12:49:57 -0300

    adicionando a funcionalidade do CartItem.jsx

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const CartItem = () => {
+const CartItem = ({ item, onUpdateCart, onRemoveFromCart }) => {
   return (
-    <div>CartItem</div>
-  )
-}
+    <div className="cart-item">
+      <h3>{item.name}</h3>
+      <p>${item.price.toFixed(2)}</p>
+      <div className="cart-buttons">
+        <input
+          type="number"
+          value={item.quantity}
+          onChange={(e) => onUpdateCart(item, parseInt(e.target.value))}
+          min="1"
+        />
+        <button onClick={() => onRemoveFromCart(item)}>Remover</button>
+      </div>
+    </div>
+  );
+};
 
 export default CartItem
