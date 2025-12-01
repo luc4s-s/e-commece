c4b5dc79f553458c7b58312b01274aa7902500ac
Lucas Gomes Silva <lucasgomessilva713@gmail.com>
2025-12-01 12:52:54 -0300

    adicionando toda as funcionalidade do componente cartalog

--- src/components/Cartalog.jsx
+++ src/components/Cartalog.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Product from "./Product";
+import productsData from "../data/products_mock.json";
 
-const Cartalog = () => {
+const Catalog = ({ onAddToCart }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // Carregando os produtos do mock JSON
+    // Em um cenário real, você poderia fazer uma requisição HTTP aqui
+    setProducts(productsData);
+  }, []);
+
   return (
-    <div>Cartalog</div>
-  )
-}
+    <div>
+      <h1>Catálogo de Produtos</h1>
+      <div className="product-container">
+        {products.map((product) => (
+          <Product
+            key={product.id}
+            product={product}
+            onAddToCart={onAddToCart}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Cartalog
